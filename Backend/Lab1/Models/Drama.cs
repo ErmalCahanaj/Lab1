64e8bc338ea0b6573f5489c132f51b0ff06e7cf7
ElmedinaMorinaa <96397014+ElmedinaMorinaa@users.noreply.github.com>
2023-02-14 21:51:54 +0100

    Two genre added
    2 cruds Hindi and Drama added

---
+++ Backend/Lab1/Models/Drama.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1.Models
+{
+    public class Drama
+    {
+        public int DramaId { get; set; }
+        public string DramaName { get; set; }
+        public string Imdb { get; set; }
+
+        public string DateOfRelease { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
---
+++ Backend/Lab1/Models/Hindi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1.Models
+{
+    public class Hindi
+    {
+
+        public int HindiId { get; set; }
+        public string HindiName { get; set; }
+        public string Imdb { get; set; }
+
+        public string DateOfRelease { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
+
+
