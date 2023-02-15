5758e94f06e7de38a5f452d7d4c100b0e57a66d1
ElmedinaMorinaa <96397014+ElmedinaMorinaa@users.noreply.github.com>
2023-02-15 20:54:45 +0100

    2 modele added
    Modelet per Vitin dhe Nordik

---
+++ Backend/Lab1/Models/Nordik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1.Models
+{
+    public class Nordik
+    {
+
+        public int NordikId { get; set; }
+        public string NordikName { get; set; }
+        public string Viti { get; set; }
+
+       
+
+        public string PhotoFileName { get; set; }
+    }
+}
+
---
+++ Backend/Lab1/Models/Viti.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lab1.Models
+{
+    public class Viti
+    {
+        public int VitiId { get; set; }
+
+        public string VitiValue { get; set; }
+    }
+}
