984fc2001463a1073b1e9fdf2b6b9782958ee9fb
ElmedinaMorinaa <96397014+ElmedinaMorinaa@users.noreply.github.com>
2023-02-16 17:31:50 +0100

    model for Romance added

---
+++ Backend/Lab1/Models/Romance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1.Models
+{
+    public class Romance
+    {
+
+        public int RomanceId { get; set; }
+        public string RomanceName { get; set; }
+        public string Imdb { get; set; }
+
+        public string DateOfRelease { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
+
