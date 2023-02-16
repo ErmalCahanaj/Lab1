2224de98f2f964dcee4f986cd409807ae7940fda
ElmedinaMorinaa <96397014+ElmedinaMorinaa@users.noreply.github.com>
2023-02-16 17:30:58 +0100

    1 controller added

---
+++ Backend/Lab1/Controllers/RomanceController.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Lab1.Models;
+
+namespace Lab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RomanceController : ControllerBase
+    {
+
+
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public RomanceController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select RomanceId, RomanceName,Imdb,
+                            convert(varchar(10),DateOfRelease,120) as DateOfRelease,PhotoFileName
+                            from
+                            dbo.Romance
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MovieAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Romance kmd)
+        {
+            string query = @"
+                           insert into dbo.Romance
+                           (RomanceName,Imdb,DateOfRelease,PhotoFileName)
+                    values (@RomanceName,@Imdb,@DateOfRelease,@PhotoFileName)
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MovieAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@RomanceName", kmd.RomanceName);
+                    myCommand.Parameters.AddWithValue("@Imdb", kmd.Imdb);
+                    myCommand.Parameters.AddWithValue("@DateOfRelease", kmd.DateOfRelease);
+                    myCommand.Parameters.AddWithValue("@PhotoFileName", kmd.PhotoFileName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Romance kmd)
+        {
+            string query = @"
+                           update dbo.Romance
+                           set RomanceName= @RomanceName,
+                            Imdb=@Imdb,
+                            DateOfRelease=@DateOfRelease,
+                            PhotoFileName=@PhotoFileName
+                            where RomanceId=@RomanceId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MovieAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@RomanceId", kmd.RomanceId);
+                    myCommand.Parameters.AddWithValue("@RomanceName", kmd.RomanceName);
+                    myCommand.Parameters.AddWithValue("@Imdb", kmd.Imdb);
+                    myCommand.Parameters.AddWithValue("@DateOfRelease", kmd.DateOfRelease);
+                    myCommand.Parameters.AddWithValue("@PhotoFileName", kmd.PhotoFileName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                           delete from dbo.Romance
+                            where RomanceId=@RomanceId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MovieAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@RomanceId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult("anonymous.png");
+            }
+        }
+
+    }
+}
+
+
